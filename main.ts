b8779f8d535f20b2264d39f585930b566746336a
yona0625 <yona0625@gmail.com>
2025-11-21 23:43:07 +0900

    *11/21일자 백업; 1. 모터 90도 정지 후 소리 끄기 블록 추가 시 음악이 다시 재생이 안 되는 문제가 있음.

--- main.ts
+++ main.ts
@@ -1,24 +1,50 @@
 input.onGesture(Gesture.TiltRight, function () {
-    basic.showIcon(IconNames.Surprised)
+    basic.showIcon(IconNames.Rollerskate)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    basic.showIcon(IconNames.Meh)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    모터_구동_중 = 모터_구동_중 == false
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
 })
 input.onSound(DetectedSound.Loud, function () {
-    input.setSoundThreshold(SoundThreshold.Loud, 255)
-    basic.showIcon(IconNames.StickFigure)
-    music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
+    input.setSoundThreshold(SoundThreshold.Loud, 230)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Heart)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
-    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.clearScreen()
     music.stopAllSounds()
 })
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (모터_구동_중 == true) {
+        pins.servoWritePin(AnalogPin.P2, 0)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P2, 180)
+        basic.pause(500)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 90)
+    }
+    basic.clearScreen()
+})
 let 현재_빛_세기 = 0
 let 모터_구동_중 = false
+모터_구동_중 = false
 basic.forever(function () {
     if (모터_구동_중 == true) {
         pins.servoWritePin(AnalogPin.P2, 0)
@@ -30,7 +56,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (input.soundLevel() >= 255) {
+    if (input.soundLevel() >= 230) {
         모터_구동_중 = 모터_구동_중 == false
         basic.pause(500)
     }
@@ -38,15 +64,11 @@ basic.forever(function () {
 basic.forever(function () {
     if (input.lightLevel() >= 30) {
         if (현재_빛_세기 == 1) {
-            music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.InBackground)
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
             basic.showString("good morning!")
         }
         현재_빛_세기 = 0
     }
-})
-basic.forever(function () {
-    let 현재_음량 = ""
-    serial.writeValue(현재_음량, input.soundLevel())
 })
 basic.forever(function () {
     if (input.lightLevel() == 0) {
@@ -56,4 +78,8 @@ basic.forever(function () {
         }
         현재_빛_세기 = 1
     }
+})
+basic.forever(function () {
+    let 현재_음량 = ""
+    serial.writeValue(현재_음량, input.soundLevel())
 })
